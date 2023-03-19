8c8cf49ac99629dbc3e9a6b8459c7cd393d43ced
xbenduan <xbenduan@outlook.com>
2023-03-19 14:22:36 +0800

    fix: 过滤dist文件夹

--- dist/assets/index.f67f9a7d.js
+++
@@ -1,1 +0,0 @@
-import{d as o,B as r,C as a}from"./arco.af438d9f.js";import{f as s,h as n}from"./vue.d426fe84.js";import"./chart.a344a590.js";const d=o({__name:"index",setup(p){const e=s(),t=n().params.path;return e.replace({path:t}),(u,m)=>(r(),a("div"))}});export{d as default};
